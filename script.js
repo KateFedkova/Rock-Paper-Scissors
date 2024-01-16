729a43bdad57ac3aa4402cd17c4704b0f2d49be7
KateFedkova <kate.fedkova@gmail.com>
2024-01-16 16:33:54 +0200

    Add eventlisteners to the other option buttons

--- script.js
+++ script.js
@@ -20,12 +20,12 @@ const computerOptions = ['rock', 'paper', 'scissors'];
 
 const gameLogic = {
   'rock': { 'beats': 'scissors', 'losesTo': 'paper' },
-  'scissor': { 'beats': 'paper', 'losesTo': 'rock' },
-  'paper': { 'beats': 'rock', 'losesTo': 'scissor' }
+  'scissors': { 'beats': 'paper', 'losesTo': 'rock' },
+  'paper': { 'beats': 'rock', 'losesTo': 'scissors' },
 };
 
-
 const checkWhoIsWinner = function (playerOption, computerOption) {
+  console.log(playerOption, computerOption);
   if (gameLogic[playerOption]['beats'] === computerOption) {
     playerScore += 1;
     playerScoreEl.textContent = playerScore;
@@ -42,6 +42,29 @@ btnRock.addEventListener('click', function () {
     computerOptions[
       Math.trunc(Math.random() * (computerOptions.length - 1)) + 1
     ];
+  computerAction.src = `images/c_${option}.jpg`;
+  checkWhoIsWinner(action, option);
+});
+
+btnPaper.addEventListener('click', function () {
+  playerAction.src = 'images/p_paper.jpg';
+  const action = 'paper';
+  const option =
+    computerOptions[
+      Math.trunc(Math.random() * (computerOptions.length - 1)) + 1
+    ];
+  computerAction.src = `images/c_${option}.jpg`;
+  checkWhoIsWinner(action, option);
+});
+
+btnScissors.addEventListener('click', function () {
+  playerAction.src = 'images/p_scissors.jpg';
+  const action = 'scissors';
+  const option =
+    computerOptions[
+      Math.trunc(Math.random() * (computerOptions.length - 1)) + 1
+    ];
+  console.log(option, action);
   computerAction.src = `images/c_${option}.jpg`;
   checkWhoIsWinner(action, option);
 });
