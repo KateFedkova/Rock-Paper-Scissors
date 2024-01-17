95a8db572c281251a3a9a23be2faca767301442d
KateFedkova <kate.fedkova@gmail.com>
2024-01-17 20:27:02 +0200

    Refactor the code

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
 
 // Select buttons
 const optionButtons = document.querySelectorAll('.option');
-const newButton = document.querySelector('.btn--new');
+const newGameButton = document.querySelector('.btn--new');
 
 // Select pictures
 const playerAction = document.getElementById('player--action');
@@ -12,6 +12,11 @@ const computerAction = document.getElementById('computer--action');
 const playerScoreEl = document.getElementById('current--player');
 const computerScoreEl = document.getElementById('current--computer');
 
+// Select name elements
+const player = document.getElementById('name--0');
+const computer = document.getElementById('name--1');
+
+// Initialize values
 let playerScore = 0;
 let computerScore = 0;
 let playing = true;
@@ -19,17 +24,24 @@ let playing = true;
 const computerOptions = ['rock', 'paper', 'scissors'];
 
 const gameLogic = {
-  rock: { beats: 'scissors', losesTo: 'paper' },
-  scissors: { beats: 'paper', losesTo: 'rock' },
-  paper: { beats: 'rock', losesTo: 'scissors' },
+  rock: {
+    beats: 'scissors',
+    losesTo: 'paper',
+  },
+  scissors: {
+    beats: 'paper',
+    losesTo: 'rock',
+  },
+  paper: {
+    beats: 'rock',
+    losesTo: 'scissors',
+  },
 };
 
 const showChosenOptions = function (playerOption) {
-  playerAction.src = `images/p_${playerOption}.jpg`;
   const computerOption =
-    computerOptions[
-      Math.trunc(Math.random() * (computerOptions.length - 1)) + 1
-    ];
+    computerOptions[Math.trunc(Math.random() * computerOptions.length)];
+  playerAction.src = `images/p_${playerOption}.jpg`;
   computerAction.src = `images/c_${computerOption}.jpg`;
   return computerOption;
 };
@@ -39,12 +51,12 @@ const checkWhoIsWinner = function (playerOption, computerOption) {
     playerScore += 1;
     playerScoreEl.textContent = playerScore;
     playerScoreEl.classList.add('current--winner');
-    document.getElementById('name--0').classList.add('player--winner');
+    player.classList.add('player--winner');
   } else if (gameLogic[playerOption].losesTo === computerOption) {
     computerScore += 1;
     computerScoreEl.textContent = computerScore;
     computerScoreEl.classList.add('current--winner');
-    document.getElementById('name--1').classList.add('player--winner');
+    computer.classList.add('player--winner');
   }
 };
 
@@ -53,31 +65,27 @@ const setWhiteBackground = function () {
   computerAction.src = 'images/white-background.jpg';
 };
 
+const resetGame = function () {
+  playing = true;
+  setWhiteBackground();
+  playerScoreEl.textContent = 0;
+  computerScoreEl.textContent = 0;
+  playerScoreEl.classList.remove('current--winner');
+  computerScoreEl.classList.remove('current--winner');
+  player.classList.remove('player--winner');
+  computer.classList.remove('player--winner');
+};
+
 for (let i = 0; i < optionButtons.length; i++) {
-  optionButtons[i].addEventListener('click', () => {
+  optionButtons[i].addEventListener('click', function () {
     if (playing) {
-      playing = false;
       const elemId = optionButtons[i].id;
       const computerOption = showChosenOptions(elemId);
+      playing = false;
       checkWhoIsWinner(elemId, computerOption);
-      setTimeout(() => {
-        setWhiteBackground();
-        playerScoreEl.classList.remove('current--winner');
-        computerScoreEl.classList.remove('current--winner');
-        document.getElementById('name--0').classList.remove('player--winner');
-        document.getElementById('name--1').classList.remove('player--winner');
-        playing = true;
-      }, 2000);
+      setTimeout(() => resetGame, 2000);
     }
   });
 }
 
-newButton.addEventListener('click', function () {
-  playerScoreEl.textContent = 0;
-  computerScoreEl.textContent = 0;
-  setWhiteBackground();
-  playerScoreEl.classList.remove('current--winner');
-  computerScoreEl.classList.remove('current--winner');
-  document.getElementById('name--0').classList.remove('player--winner');
-  document.getElementById('name--1').classList.remove('player--winner');
-});
+newGameButton.addEventListener('click', resetGame);
